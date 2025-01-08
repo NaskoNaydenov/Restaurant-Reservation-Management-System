28f6efe86773a7fe8cab0381716f4fa2bbe53e76
KeepeR03 <nasko.naydenoff@gmail.com>
2025-01-08 23:03:23 +0200

    New change

--- RestorantReservations/Controllers/HomeController.cs
+++ RestorantReservations/Controllers/HomeController.cs
@@ -22,6 +22,10 @@ namespace RestorantReservations.Controllers
         {
             return View();
         }
+        public IActionResult New()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
