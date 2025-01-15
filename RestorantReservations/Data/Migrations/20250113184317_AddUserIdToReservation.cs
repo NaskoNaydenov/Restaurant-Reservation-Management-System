1a529c8d41e9bd5ce724a6a981066effd080ad5f
Kix13 <79169226+Kix13@users.noreply.github.com>
2025-01-15 21:02:50 +0200

    Register Page Design

---
+++ RestorantReservations/Data/Migrations/20250113184317_AddUserIdToReservation.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RestorantReservations.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserIdToReservation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Reservation",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Reservation");
+        }
+    }
+}
